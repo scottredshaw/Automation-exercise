d6e517c1f707fc18e328e705a395a4bd52be4001
scottredshaw <62606413+scottredshaw@users.noreply.github.com>
2024-09-17 10:46:28 +0100

    Completed question 1
    Unit tests are passing using Junit in eclipse IDE

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,9 +1,23 @@
 package words;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        throw new UnsupportedOperationException();
+    	// throw new UnsupportedOperationException();
+    	String[] words = sentence.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+    	List<String> indi_words = new ArrayList<>();
+        for(int i = 0; i < words.length; i++) {
+        	if(indi_words.contains(words[i]) == false) {
+        		indi_words.add(words[i]);
+        	}
+        }
+        return indi_words;
+        
+        
+        // first thing is to split all the strings into individual words
+        // make all the words lower case
+        // store all the individual words in a list 
     }
 }
